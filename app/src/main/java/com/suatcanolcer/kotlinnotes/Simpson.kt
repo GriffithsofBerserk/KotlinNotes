58963fdd65fef8d9bceb84dcb8fdb0055474c4d2
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2024-07-21 21:47:06 +0300

    classes and primary-secondary constructors

---
+++ app/src/main/java/com/suatcanolcer/kotlinnotes/Simpson.kt
@@ -0,0 +1,18 @@
+package com.suatcanolcer.kotlinnotes
+//Primary Constructor
+class Simpson(var name: String,var age: Int,var job: String) {
+
+   /*
+    var name = ""
+    var age = 0
+    var job = ""
+
+       //Seconday Constructor
+    constructor(name: String,age: Int,job: String) {
+        this.name = name
+        this.age = age
+        this.job = job
+
+    }
+    */
+}
